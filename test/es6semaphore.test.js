0ae854d81db7487d33a778a97fb14292d77b529f
debonet <jeremy@buildingmachines.com>
2022-08-12 22:25:38 -0400

    fix test

--- test/es6semaphore.test.js
+++ test/es6semaphore.test.js
@@ -6,14 +6,14 @@ if (!test){
 	}
 }
 
+function sleep( dtm ){
+	return new Promise( fOk => setTimeout( fOk, dtm ));
+}
+
 test("ensure semaphore works", async () => {
 
 	let s="";
 
-	function sleep( dtm ){
-		return new Promise( fOk => setTimeout( fOk, dtm ));
-	}
-	
 	async function f(ch) {
 		await semaphore.wait();
 		await sleep(100);
@@ -25,6 +25,43 @@ test("ensure semaphore works", async () => {
 	
 	// these will run serially, one per second
 	f('a');
+	f('b');
+	f('c');
+	f('d');
+	s+="queued,";
+	await semaphore.when(c => c==1 );
+	s+="one,";
+	await semaphore.whenAll();
+	s+="all";
+
+	
+	expect(s).toBe( "queued,a,b,c,one,d,all" );
+});
+		 
+
+	
+
+test("simple semaphore when", async () => {
+
+	let s="";
+
+	function sleep( dtm ){
+		return new Promise( fOk => setTimeout( fOk, dtm ));
+	}
+	
+	async function f(ch) {
+		await semaphore.wait();
+		await sleep(1000);
+		s += ch + ",";
+		semaphore.signal();
+	}
+	
+	const semaphore = new Semaphore(2);
+	
+	// these will run serially, one per second
+	f('a');
+	await semaphore.whenAny();
+	
 	f('b');
 	f('c');
 	f('d');
